3ad070b6cb04caca7bad96c41cf7fab67dac33cd
FedericoCantarelli <fede.cantarelli98@gmail.com>
2023-01-04 16:04:33 +0100

    utils definitivo

--- code/new code/utils.py
+++ code/new code/utils.py
@@ -1,25 +1,17 @@
 import random
-import pandas as pd
-
-# Parameters
-max_x = 75  # Extreme right point of tugger train path
-
 
 def read_line_info(path: str, conversion_factor=60):
     """
-    path:str path to the file
-    conversion_factor: adjust measure unit
-    Read a file and returns line coordinates, cycle times[min] and weights for each line.
+    :param path: str path to the file
+    :param conversion_factor: adjust measure unit, default is 60 (from minutes to seconds)
+    :return: Tuple (x, y, cycle times, UL weights)
 
-    Return
-    -------
-    Tuple (x, y, cycle times, UL weights)
+    Read file and return lines info.
     """
     x = []
     y = []
     cycle_times = []
     weights = []
-
     with open(path, "r") as f:
         f.seek(0)
         for line in f.readlines()[1:]:
@@ -32,17 +24,18 @@ def read_line_info(path: str, conversion_factor=60):
     return x, y, cycle_times, weights
 
 
-def compute_distance(x1: float, x2: float, y1: float, y2: float, max_x = max_x):
+def compute_distance(x1: float, x2: float, y1: float, y2: float, max_x: float = 75):
     """
-    x1:float, x2:float, y1:float, y2:float, max_x
+    :param x1: float, x of first point
+    :param x2: float, x of second point
+    :param y1: float, y of first point
+    :param y2: float, y of second point
+    :param max_x: correction factor. Default is 75
+    :return: rect distance between two points
 
-    Returns rectilinear distance between two points. The x_max is the correction factor to avoid the miscalculation of distance between station 3 e 4.
-
-    Return
-    -------
-    float distance
+    Returns rectilinear distance between two points. The x_max is the correction factor to avoid the miscalculation
+    of distance between station 3 e 4.
     """
-
     if y2 > y1:
         return abs(max_x - x1) + (max_x - x2) + abs(y1 - y2)
     else:
@@ -51,14 +44,12 @@ def compute_distance(x1: float, x2: float, y1: float, y2: float, max_x = max_x):
 
 def compute_speed(weight: float, max_weight: float = 2000):
     """
-    weight:float
+    :param weight: float, carried weight
+    :param max_weight: float, maximum weight. Default is 2000kg
+    :return: float speed [m/s]
 
     Returns the speed [m/s] of a tugger considering the weight it is transporting. Here, we assume that the speed
     decreases linearly with the load. Assume by a rule of thumb that speed_min = 1.2 m/s and speed_max = 1.6 m/s
-
-    Returns
-    -------
-    float speed [m/s]
     """
     speed_min = 1.2
     speed_max = 1.6
@@ -66,20 +57,21 @@ def compute_speed(weight: float, max_weight: float = 2000):
     return speed
 
 
-def compute_time(distance: float, speed: float, nextline: int = 0, random_flag=True):
+def compute_time(distance: float, speed: float, nextline: int = 0, random_flag: bool=True):
     """
-    distance:float, speed:float
+    :param distance: float, distance to be travelled
+    :param speed: float, speed of the tuggers
+    :param nextline: int, id of next line
+    :param random_flag: bool, if set True consider process stochasticity. Default is True
+    :return: times [s]
 
     Returns the cartesians' movements' time  considering distance [m] to be travelled, speed, and the
     plane's area which the tugger moves on. The time is computed adding a random
     delay depending on the final destination and on the distance.
     Furthermore a fixed amount corresponding to acceleration and decelaration has been added.
     The function returns time in seconds.
-
-    Return
-    -------
-    float time [s]
     """
+
     # fixed amount for accelerating is taken from LTX_20_T04_50_iGo_EN_TD.pdf page 2, acceleration time for the
     # truck with the same towing capacity
 
@@ -95,36 +87,39 @@ def compute_time(distance: float, speed: float, nextline: int = 0, random_flag=T
             return distance / speed + 8
 
 
-def compute_energy(time: float, consumption=2.6):
+def compute_energy(time: float, consumption: float = 2.6):
     """
-    :param time: float
-    :param consumption: [kwh]
-    Consumption is defined as an average consumption by EN standard.
-    -----
+    :param time: float, time in seconds
+    :param consumption: consumption in [kWh]. Default is 2.6
     :return: Energy Consumed
-    """
 
+    Consumption is defined as an average consumption by EN standard.
+    """
     energy = consumption / 3600 * time
     return energy
 
 
-def compute_energy_loading(weight: float):
+def compute_energy_loading(weight: float, disp: float = 1.2, frict: float = 1.2):
     """
-    Parameters
-    weight:float
+    :param weight: float, UL weight
+    :param disp: displacement in meters to compute energy used. Default is 1.2
+    :param frict: friction coefficient. Default is 1.2
+    :return: energy consumed [kWh]
 
-    Returns energy consumed to load and unload the pallet loads, computed on the distance moved
-    and the weight of the pallet
-
-    Returns
-    float: energy consumed [kWh]
-
+    Returns energy consumed to load and unload the pallet loads, computed on the displacement moved
+    and the weight of the pallet.
     """
-    energy = 1.2 * weight * 9.81 / 3600000 * 1.2
+    energy = disp * weight * 9.81 / 3600000 * frict
     return energy
 
 
 def progress(percent=0, width=40):
+    """
+    :param percent: percentage of completion
+    :param width: bar size. Default is 40.
+    :return: None
+    Print progress bar.
+    """
     left = width * percent // 100
     right = width - left
     tags = "=" * left
@@ -135,8 +130,9 @@ def progress(percent=0, width=40):
 
 def read_charging_phases(path: str):
     """
-    :param path: path of the csv file
+    :param path: str, path of the csv file
     :return: dict with all the charging data
+    Read the file with the charging times
     """
     charge = dict()
     with open(path, "r") as f:
@@ -150,9 +146,12 @@ def read_charging_phases(path: str):
 def compute_charging_time(charge: dict, remaining_energy: float, tot_cap: float):
     """
     :param charge: Dictionary with charging stages
-    :param remaining_energy: Remaining energy of the tugger
-    :param tot_cap: Total capacity of the battery
+    :param remaining_energy: float, Remaining energy of the tugger
+    :param tot_cap: float, Total capacity of the battery
     :return: Time to reach full charge
+
+    Compute time to reach full charge by assuming linear interpolation between different percentage. Under expert advice,
+    we used iphone pro charging function phases.
     """
     percentage = remaining_energy/tot_cap
     for i in charge.keys():
@@ -174,13 +173,19 @@ def compute_charging_time(charge: dict, remaining_energy: float, tot_cap: float)
 
 def check_behavior(*args):
     """
-    Function to perform a true XOR
+    :param args: parameters
+    :return: True if just one args is True or if all args are False. Otherwise return False
+
+    Check if simulation parameters have been defined properly.
     """
-    return sum(args) == 1 or sum(args)==0
+    return sum(args) == 1 or sum(args) == 0
 
 
 def check_combination(*args):
     """
+    :param args: Lists of hyperparameters
+    :return: True if the more than one parameter was declared in hyperparameters lists, False otherwise
+
     Function that return True if the more than one parameter was declared in hyperparameters lists.
     """
     s_ = 0
